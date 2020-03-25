fa806296f7c3199cc7ed0cd445ae1b4d5f558bbc
JaeKyung LEE <jklee78e@gmail.com>
2020-03-26 00:00:27 +0900

    on the way to use BT

--- app/src/main/java/com/example/test1/MainActivity.kt
+++ app/src/main/java/com/example/test1/MainActivity.kt
@@ -7,28 +7,48 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.annotation.UiThread
 import androidx.fragment.app.FragmentActivity
+import app.akexorcist.bluetotohspp.library.BluetoothSPP
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.*
 import com.naver.maps.map.NaverMap.LAYER_GROUP_MOUNTAIN
+import com.naver.maps.map.overlay.Marker
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val bt by lazy{ BluetoothSPP(this)}
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         Log.d("cmd", "Runned")
+
         val options = NaverMapOptions()
             .camera(CameraPosition(LatLng(35.1798159, 129.0750222), 16.0))
             .mapType(NaverMap.MapType.Hybrid)
             .enabledLayerGroups(NaverMap.LAYER_GROUP_MOUNTAIN)
+
         val fm = supportFragmentManager
+
         val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
             ?: MapFragment.newInstance(options).also {
                 fm.beginTransaction().add(R.id.map, it).commit()
             }
         mapFragment.getMapAsync(this)
-    }
+
+        if(!bt.isBluetoothAvailable()) {
+            Toast.makeText(this, "BT Ready", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+//https://blog.codejun.space/13
+
+ /*       bt.setOnDataReceivedListener(BluetoothSPP.OnDataReceivedListener(){
+            public void onDataReceived(var data : Byte[]; var message : String){
+                Toast.makeText(this,message, toast.LENGTH_SHORT).show()
+            }
+        })*/
+}
 
     @UiThread
     override fun onMapReady(naverMap: NaverMap) {
@@ -48,11 +68,21 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
             .include(LatLng(37.5590777, 126.974617))
             .build()
     */
+/*        val marker = Marker()
+        val locationOverlay=naverMap.locationOverlay
+        marker.map = naverMap
+*/
+        //infoWindow.open(marker)
+
         naverMap.setOnMapClickListener { point, coord ->
             Toast.makeText(
                 this, "${coord.latitude}, ${coord.longitude}",
                 Toast.LENGTH_SHORT
             ).show()
+            val marker = Marker()
+            marker.position = LatLng(coord.latitude, coord.longitude)
+            marker.map = naverMap
+
         }
     }
 
